4834074dd2b1ac15e2389df4fba6cb248a050cc5
loresjoberg <loresjoberg@gmail.com>
2024-12-21 13:06:19 -0800

    Changing color of underline

--- resources/js/components/Link/NavLink/NavBarLink.tsx
+++ resources/js/components/Link/NavLink/NavBarLink.tsx
@@ -5,13 +5,12 @@ import classes from './NavLinkClasses.module.css';
 
 export function NavBarLink({ text, href }: SiteLinkProps) {
     return (
-        <Anchor component={Link} href={href}>
+        <Anchor component={Link} href={href} c="secondary.9">
             <Box
                 className={classes.navLink}
                 ff="PT Sans Narrow"
                 td="none"
                 fw="bold"
-                c="secondary.9"
                 lh={2}
             >
                 {text}
