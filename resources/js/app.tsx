d4ddc0b59720a3a6b73d28fb5a43eeac20029be7
loresjoberg <loresjoberg@gmail.com>
2024-12-21 12:10:03 -0800

    Trying Mantine classes again

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,7 +5,7 @@ import './bootstrap';
 
 
 import { createInertiaApp } from '@inertiajs/react';
-import { createTheme, MantineProvider, MantineThemeOverride } from '@mantine/core';
+import { Anchor, createTheme, MantineProvider, MantineThemeOverride } from '@mantine/core';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
@@ -17,7 +17,14 @@ const theme = createTheme({
         'primary': ['#DCE7E9', '#B9CFD3', '#96B6BC', '#739EA6', '#5E8095', '#4A6185', '#354374', '#43577F', '#3C4D7A', '#354374'],
         'secondary': ['#DED4E4', '#C8B8D2', '#B29BC1', '#9C7FAF', '#8A6EA2', '#775C96', '#654B89', '#523F80', '#3F3277', '#2C266E'],
         'highlight': ['#F8F7EE', '#F1F0DE', '#EAE8CD', '#E3E1BD', '#DCD9AC', '#D5D29C', '#CECA8B', '#B7B47C', '#A09D6C', '#89875D']
-    }
+    },
+    components: {
+        Anchor: Anchor.extend({
+            defaultProps: {
+                underline: 'hover',
+            },
+        }),
+    },
 } as MantineThemeOverride);
 
 
