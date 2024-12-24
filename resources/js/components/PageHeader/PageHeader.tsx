eb9f021be928c17e8331fc055605e8f7ca5272a6
loresjoberg <loresjoberg@gmail.com>
2024-12-24 15:38:45 -0800

    Adding mobile menu

--- resources/js/components/PageHeader/PageHeader.tsx
+++ resources/js/components/PageHeader/PageHeader.tsx
@@ -11,7 +11,7 @@ interface PageHeaderProps {
 
 export function PageHeader({ navLinks, stageWidth }: PageHeaderProps) {
     return (<Container mt="1em">
-        <Box hiddenFrom="sm" mb="1em" pb="1em" style={{borderBottom: "4px solid #00ff00"}}>
+        <Box hiddenFrom="sm" mb="1em" pb="1em" style={{borderBottom: "4px solid #0000ff"}}>
             <MobileBanner navLinks={navLinks}/>
         </Box>
         <Box visibleFrom="sm">
