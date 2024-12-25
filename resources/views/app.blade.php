edb98ef129d7c7388234bbc5ea7f60b6a49ea54f
loresjoberg <loresjoberg@gmail.com>
2024-12-25 10:44:52 -0800

    Added card images in head

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta property="og:image" content="https://badgods.com/storage/thumbnails/one-word-cute-thumb.png" inertia="">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
