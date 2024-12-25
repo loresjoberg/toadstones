24519aabf0caf0a071666d4720501c09825f0402
loresjoberg <loresjoberg@gmail.com>
2024-12-25 14:28:37 -0800

    Attempting SSR

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
-            ssr: 'resources/js/ssr.js',
+            ssr: 'resources/js/ssr.tsx',
     refresh: true,
         }),
         react(),
