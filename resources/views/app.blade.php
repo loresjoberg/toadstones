7ec4af1d98ada68fa5bc46f1cc23e976944774dd
loresjoberg <loresjoberg@gmail.com>
2024-12-25 14:18:28 -0800

    Working client-size

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,20 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:image" content={{'https://' . Request::httpHost() . '/storage/thumbnails/' . explode('/', Request::path())[1] . '-thumb.png'}}>
-    <meta property="twitter:image" content={{'https://' . Request::httpHost() . '/storage/thumbnails/' . explode('/', Request::path())[1] . '-thumb.png'}}>
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <!-- TODO: Make this work better -->
+    @if (Request::path() == '/')
+        <meta property="og:image"
+              content={{'https://' . Request::httpHost() . '/storage/ui/bad-gods-logo.png'}}>
+        <meta property="twitter:image"
+              content={{'https://' . Request::httpHost() . '/storage/ui/bad-gods-logo.png'}}>
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    @else
+        <meta property="og:image"
+              content={{'https://' . Request::httpHost() . '/storage/thumbnails/' . explode('/', Request::path())[1] . '-thumb.png'}}>
+        <meta property="twitter:image"
+              content={{'https://' . Request::httpHost() . '/storage/thumbnails/' . explode('/', Request::path())[1] . '-thumb.png'}}>
+
+    @endif
     @routes
     @viteReactRefresh
     @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
