16ac73ea7ec40bd1690a662b6a4a21225662e25f
loresjoberg <loresjoberg@gmail.com>
2024-12-25 17:23:01 -0800

    Making TextRule fancier

--- resources/js/components/TextRule/TextRule.tsx
+++ resources/js/components/TextRule/TextRule.tsx
@@ -6,8 +6,8 @@ interface TextRuleProps {
 }
 
 export function TextRule({ label }: TextRuleProps) {
-    return <Flex direction="row" gap="0.5rem" align="center">
-        <Box w="0.5rem" style={{flexGrow: 0, borderRight: "4px solid #B29BC1"}}>
+    return <Flex direction="row" gap="0.25rem" align="center">
+        <Box w="1rem" style={{flexGrow: 0, borderRight: "4px solid #B29BC1"}}>
             <hr style={{border: 0, height: "4px", backgroundColor: "#B29BC1"}}/>
         </Box>
         <Text fw="bold" pr="0.1rem" fz="1.375rem" c="primary.6" style={{flexGrow: 0}}>{label}</Text>
