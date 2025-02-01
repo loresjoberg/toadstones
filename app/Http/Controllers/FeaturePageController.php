aebade45b2e0965089b33261e5f5a948b6110248
loresjoberg <loresjoberg@gmail.com>
2025-02-01 14:18:37 -0800

    Only live features

--- app/Http/Controllers/FeaturePageController.php
+++ app/Http/Controllers/FeaturePageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Feature;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -12,9 +13,8 @@ class FeaturePageController extends Controller
 {
     public function show($slug): Response
     {
-        $feature = Feature::where('slug', $slug)->get();
-        Log::debug('FeaturePageController::show.$feature', [$feature]);
-        return Inertia::render('Features/FeaturePage', ['feature' => $feature[0], 'features' => Feature::all()]);
+        $features = Feature::where('slug', $slug)->where('launch', '<', Carbon::now())->get();
+        return Inertia::render('Features/FeaturePage', ['feature' => $features[0], 'features' => $features]);
     }
 
 }
